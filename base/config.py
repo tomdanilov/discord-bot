350a2cecdf7343d6c60c911557fc6c5ed95eaea0
Tom Danilov <66553758+tomdanilov@users.noreply.github.com>
2020-07-05 16:26:59 +0300

    First things first

---
+++ base/config.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from dataclasses import dataclass
+import json
+
+
+@dataclass
+class Config:
+    token: str
+    cogs: list
+
+    @staticmethod
+    def load(filename: str='config.json') -> Config:
+        with open(filename, 'r') as file:
+            config_json = file.read()
+
+        config_dict = json.loads(config_json)
+        return Config(**config_dict)
